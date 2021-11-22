92b5467aa8b72e265c179f525e4708474415f249
Steven Chang <59789081+stevendee37@users.noreply.github.com>
2021-11-22 12:54:59 -0800

    Added comments to mipsmulti.v

--- mipsmulti.v
+++ mipsmulti.v
@@ -38,10 +38,12 @@ module controller(input       clk, reset,
   wire [1:0] aluop;
   wire branch, pcwrite;
 
+  // Instantiates main decoder and ALU decoder, assigns appropriate inputs/outputs
   maindec md(clk, reset, op, pcwrite, memwrite, irwrite, regwrite, alusrca, branch, iord,
 	     memtoreg, regdst, alusrcb, pcsrc, aluop);
   aludec ad(funct, aluop, alucontrol);
 
+  // Determines pcen based on the values of pcwrite, branch, and zero. 
   assign pcen = pcwrite | (branch & zero);
  
 endmodule
@@ -67,28 +69,47 @@ module datapath(input        clk, reset,
   wire [31:0] signimmsh;
   wire [31:0] wd3, rd1, rd2;
 
+  // Assigns op to the 6 most significant bits of instr
   assign op = instr[31:26];
+  // Assigns funct to the 6 least significant bits of instr
   assign funct = instr[5:0];
 
+  // Instantiation of PC register, with width of 32
   flopenr #(32) pcreg(clk, reset, pcen, pcnext, pc);
+  // Instantiation of 2:1 MUX that determines whether to get new instructino from PC 
+  // or continues instruction from the previous cycle
   mux2 #(32) adrmux(pc, aluout, iord, adr);
+  // Instantiation of both instruction and data registers
   flopenr #(32) instrreg(clk, reset, irwrite, readdata, instr);
   flopr #(32) datareg(clk, reset, readdata, data);
   
+  // Instantiation of 2:1 MUX which connects to A3 of register file, parses instr
+  // to get two instruction inputs
   mux2 #(5) regdstmux(instr[20:16], instr[15:11], regdst, writereg);
+  // Instantiation of 2:1 MUX which connects to WD3 of register file, input determined by MemtoReg
   mux2 #(32) wdmux(aluout, data, memtoreg, wd3);
+  // Instantiation of Register file
   regfile rf(clk, regwrite, instr[25:21], instr[20:16], writereg,
 	     wd3, rd1, rd2);
+  // Instantiation of sign extension module, used if instruction uses immediates
   signext se(instr[15:0], signimm);
+  // Instantiation of shift left module, shifts sign extended number to the left 2
   sl2 immsh(signimm, signimmsh);
+  // Instantiation of flip flop for both the output RD1 and RD2 of the register file	
   flopr #(32) areg(clk, reset, rd1, a);
   flopr #(32) breg(clk, reset, rd2, writedata);
+  // Instantiation of 2:1 MUX for output A from the register file flip flop, outputs to ALU
   mux2 #(32) srcamux(pc, a, alusrca, srca);
+  // Instantiation of 4:1 MUX for output B from the register file flip flop, also takes immediate inputs
+  // and outputs to ALU
   mux4 #(32) srcbmux(writedata, 32'b100, signimm, signimmsh, alusrcb, srcb);
   
+  // Instantiation of ALU, takes SrcA and SrcB as inputs. Function determined by input of ALUControl
   ALU alu(srca, srcb, alucontrol, aluresult, zero);
   
+  // Instantiation of flip flop connected to output of ALU
   flopr #(32) alureg(clk, reset, aluresult, aluout);
+  // Instantiation of 3:1 MUX, outputs to beginning of datapath of processor
   mux3 #(32) pcmux(aluresult, aluout, {pc[31:28], instr[25:0], 2'b00}, pcsrc, pcnext);
 
 endmodule
